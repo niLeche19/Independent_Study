356576a50f8d571b9e3830c57754f3e47e0a1779
niLeche19 <nicholaslee@hanovernorwichschools.org>
2019-10-19 01:46:54 +0100

    modified:   testspace.py

--- testspace.py
+++ testspace.py
@@ -1,4 +1,5 @@
 #this is the space to test new code
+from time import sleep
 import RPi.GPIO as iop
 iop.setmode(iop.BCM)
 pins = (2,3,4,17,22,27,10,9,11,5,6,13,19,26,21,20)
@@ -7,11 +8,14 @@ for i in pins:
 
 for i in pins:
 	iop.setup(i, iop.IN)
-	print ("pin " + i + "is setup :)")
+	print ("pin " + str(i) + "is setup :)")
 
 try:
 	while True:
-		if iop.input(2)==1:
+		if iop.input(2)==0:
 			print ("Closed")
+		else:
+			print ("Open")
+		sleep(0.2)
 finally:
 	iop.cleanup()
