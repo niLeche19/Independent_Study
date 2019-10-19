4b2ba74ee0ae92193c805b823010a744515bb9da
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-10-18 20:34:10 -0400

    Update testspace.py

--- testspace.py
+++ testspace.py
@@ -4,15 +4,14 @@ iop.setmode(iop.BCM)
 pins = (2,3,4,17,22,27,10,9,11,5,6,13,19,26,21,20)
 for i in pins:
 	print (i)
+
 for i in pins:
 	iop.setup(i, iop.IN)
+	print ("pin " + i + "is setup :)")
 
 try:
 	while True:
-		for i in pins:
-			if iop.input(i)==0:
-				print ("Closed")
-			else:
-				print ("Open")
+		if iop.input(2)==1:
+			print ("Closed")
 finally:
 	iop.cleanup()
