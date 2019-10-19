4cefe41de73e7df2aed3ffc15a1cca45760ac7fe
niLeche19 <nicholaslee@hanovernorwichschools.org>
2019-10-19 02:03:57 +0100

    modified:   testspace.py

--- testspace.py
+++ testspace.py
@@ -3,12 +3,12 @@ from time import sleep
 import RPi.GPIO as iop
 iop.setmode(iop.BCM)
 pins = (2,3,4,17,22,27,10,9,11,5,6,13,19,26,21,20)
-for i in pins:
-	print (i)
+#for i in pins:
+#	print (i)
 
 for i in pins:
 	iop.setup(i, iop.IN)
-	print ("pin " + str(i) + "is setup :)")
+#	print ("pin " + str(i) + "is setup :)")
 
 try:
 	while True:
