dae7048e0810c4ac6a4f6f08381edecf2d368299
niLeche19 <nicholaslee@hanovernorwichschools.org>
2019-10-19 01:19:51 +0100

    Changes to be committed: 	modified:   testspace.py

--- testspace.py
+++ testspace.py
@@ -1,10 +1,18 @@
 #this is the space to test new code
+import RPi.GPIO as iop
+iop.setmode(iop.BCM)
+pins = (2,3,4,17,22,27,10,9,11,5,6,13,19,26,21,20)
+for i in pins:
+	print (i)
+for i in pins:
+	iop.setup(i, iop.IN)
 
-strone = input("what would you like me to break down?")
-word = ""
-for i  in strone:
-	if i != " ":
-		word.append(i)
-	else:
-		print(word)
-		word = ""
+try:
+	while True:
+		for i in pins:
+		if iop.input(i)==0:
+			print ("Closed")
+		else:
+			print ("Open")
+finally:
+	iop.cleanup()
