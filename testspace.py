d2571a164914d96b8ed4a2c34343cfe63fb4e169
niLeche19 <nicholaslee@hanovernorwichschools.org>
2019-10-20 01:23:31 +0100

    modified:   testspace.py

--- testspace.py
+++ testspace.py
@@ -2,7 +2,7 @@
 from time import sleep
 import RPi.GPIO as iop
 iop.setmode(iop.BCM)
-pins = (2,3,4,17,22,27,10,9,11,5,6,13,19,26,21,20)
+pins = (27,2,3,4,17,22,10,9,11,5,6,13,19,26,21,20)
 #for i in pins:
 #	print (i)
 
@@ -16,7 +16,7 @@ try:
 		if iop.input(2)==0:
 			print ("Closed")
 			sleep(0.1)
-		elif iop.input(27)==0:
+		if iop.input(27)==0:
 			print ("pin13!")
 			sleep(0.1)
 		else:
