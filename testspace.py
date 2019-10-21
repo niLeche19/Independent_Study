908f718c33fa661e0d64cd89ee14ded85da340de
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-10-20 20:12:09 -0400

    Update testspace.py

--- testspace.py
+++ testspace.py
@@ -1,16 +1,23 @@
 #this is the space to test new code
+"""
+#########################################################################
+Author: Nick Lee							#
+Project: Macro keyboard							#
+#########################################################################
+"""
+
 from time import sleep
 import RPi.GPIO as iop
+import pyautoguy as pag
+
 iop.setmode(iop.BCM)
 pins = (27,2,3,4,17,22,10,9,11,5,6,13,19,26,21,20)
+tstpins = (2,11,13,27)
+keysdown = []
 
 for i in pins:
 	iop.setup(i, iop.IN, pull_up_down=iop.PUD_UP)
 	print ("pin " + str(i) + "is setup :)")
-
-tstpins = (2,11,13,27)
-keysdown = []
-
 
 def lstscn (l, n):
 	fls = 0
@@ -22,14 +29,12 @@ def lstscn (l, n):
 	if fls == 0:
 		return (False)
 	else:
-		return (True)
-		
+		return (True)	
 
 try:
 	while True:
-		lstscn((1,2,3,4), 4)
 		pressed = 0
-		#print(keysdown)
+		
 		for i in tstpins:
 			fals = 1
 			if iop.input(i)==0:
@@ -41,30 +46,6 @@ try:
 			elif iop.input(i)==1:
 				if lstscn(keysdown, i) == True:
 					keysdown.remove(i)
-		
-		#print (lstscn(keysdown, i), keysdown, i)
-		
-		"""
-		if pressed == 0:
-			print ("All open")
-		else:
-			print (keysdown)
 
-		sleep(0.1)
-		
-		
-		if iop.input(27)==0:
-			print ("Closed")
-			sleep(0.1)
-		elif iop.input(2)==0:
-			print ("pin2!")
-			sleep(0.1)
-		elif iop.input(13)==0:
-			print ("pin13!")
-			sleep(0.1)
-		else:
-			print ("Open")
-			sleep(0.1)
-		"""
 finally:
 	print ("Goodbye!")
