ae8c68005f9fe73a76f6c69b79af7c870949c852
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-10-22 18:40:37 -0400

    Update givekeys.py

--- givekeys.py
+++ givekeys.py
@@ -2,4 +2,4 @@
 This file will take a string of key inputs and tell you what you will need to input into the next step.
 //\\//\\//\\//\\//\\//\\//\\//\\"""
 
-from pynput.keyboard import Controller
+import pynput
