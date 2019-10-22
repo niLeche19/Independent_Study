d1b840b3f502a613f5eeb61dfcbbaa68693f2d36
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-10-22 18:57:44 -0400

    Update givekeys.py

--- givekeys.py
+++ givekeys.py
@@ -1,5 +1,5 @@
 """//\\//\\//\\//\\//\\//\\//\\//\\
 This file will take a string of key inputs and tell you what you will need to input into the next step.
 //\\//\\//\\//\\//\\//\\//\\//\\"""
-#import pyautogui as pag
-from pynput.keyboard import Key, Controller
+import pyautogui as pag
+#from pynput.keyboard import Key, Controller
