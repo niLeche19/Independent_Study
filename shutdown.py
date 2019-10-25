1b23d69fee292a47e557612bebffd4a918fc5c3f
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-10-25 18:26:16 -0400

    Create shutdown.py

---
+++ shutdown.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as iop
+from time import sleep
+from os import system
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+while True:
+	if iop.input(20):
+		print("Shutting down :)")
+		system('sudo shutdown now')
+iop.cleanup
