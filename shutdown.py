03c40de4e492c6f9ef67cff5872ea379c2399932
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-10-26 12:15:17 -0400

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -5,7 +5,9 @@ from os import system
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
-	if iop.input(20):
-		print("Shutting down :)")
-		system('sudo shutdown now')
+	if iop.input(20) == 0:
+		sleep(1)
+		if iop.input(20) == 0:
+			print("Shutting down :)")
+			system('sudo shutdown now')
 iop.cleanup
