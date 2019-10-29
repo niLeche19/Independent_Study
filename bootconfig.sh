59be892b63626253447c1e16bb498e01de4cfa31
niLeche19 <nicholaslee@hanovernorwichschools.org>
2019-10-29 00:00:52 -0400

    modified:   bootconfig.sh

--- bootconfig.sh
+++ bootconfig.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Create gadget
-mkdir /sys/kernel/config/usb_gadget/mykeyboard
-cd /sys/kernel/config/usb_gadget/mykeyboard
+mkdir /sys/kernel/config/usb_gadget/macroboy
+cd /sys/kernel/config/usb_gadget/macroboy
 
 # Add basic information
 echo 0x0100 > bcdDevice # Version 1.0.0
@@ -17,8 +17,8 @@ echo 0x1d6b > idVendor # Linux Foundation
 # Create English locale
 mkdir strings/0x409
 
-echo "My manufacturer" > strings/0x409/manufacturer
-echo "My virtual keyboard" > strings/0x409/product
+echo "Nick Lee" > strings/0x409/manufacturer
+echo "Independent study macro keyboard" > strings/0x409/product
 echo "0123456789" > strings/0x409/serialnumber
 
 # Create HID function
@@ -35,7 +35,7 @@ mkdir configs/c.1/strings/0x409
 
 echo 0x80 > configs/c.1/bmAttributes
 echo 200 > configs/c.1/MaxPower # 200 mA
-echo "Example configuration" > configs/c.1/strings/0x409/configuration
+echo "I don't know what this text is for" > configs/c.1/strings/0x409/configuration
 
 # Link HID function to configuration
 ln -s functions/hid.usb0 configs/c.1
