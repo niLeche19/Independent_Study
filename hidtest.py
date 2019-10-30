26ec83111686092062585a3f86a48033ecf11f00
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-10-29 22:13:14 -0400

    Update hidtest.py

--- hidtest.py
+++ hidtest.py
@@ -1,39 +1,46 @@
 #!/usr/bin/env python3
+
 NULL_CHAR = chr(0)
 
 def write_report(report):
     with open('/dev/hidg0', 'rb+') as fd:
         fd.write(report.encode())
 
-# Press a
-write_report(NULL_CHAR*2+chr(4)+NULL_CHAR*5)
-# Release keys
-write_report(NULL_CHAR*8)
-# Press SHIFT + a = A
-write_report(chr(32)+NULL_CHAR+chr(4)+NULL_CHAR*5)
 
-# Press b
-write_report(NULL_CHAR*2+chr(5)+NULL_CHAR*5)
-# Release keys
-write_report(NULL_CHAR*8)
-# Press SHIFT + b = B
-write_report(chr(32)+NULL_CHAR+chr(5)+NULL_CHAR*5)
+# H (press shift and H)
+write_report(chr(32)+NULL_CHAR+chr(11)+NULL_CHAR*5)
 
-# Press SPACE key
+# e
+write_report(NULL_CHAR*2+chr(8)+NULL_CHAR*5)
+
+# ll
+write_report(NULL_CHAR*2+chr(15)+NULL_CHAR*5)
+write_report(NULL_CHAR*8)
+write_report(NULL_CHAR*2+chr(15)+NULL_CHAR*5)
+
+# o
+write_report(NULL_CHAR*2+chr(18)+NULL_CHAR*5)
+
+# SPACE
 write_report(NULL_CHAR*2+chr(44)+NULL_CHAR*5)
 
-# Press c key
-write_report(NULL_CHAR*2+chr(6)+NULL_CHAR*5)
-# Press d key
+# W (press shift and W)
+write_report(chr(32)+NULL_CHAR+chr(26)+NULL_CHAR*5)
+
+# o
+write_report(NULL_CHAR*2+chr(18)+NULL_CHAR*5)
+
+# r
+write_report(NULL_CHAR*2+chr(21)+NULL_CHAR*5)
+
+# l
+write_report(NULL_CHAR*2+chr(15)+NULL_CHAR*5)
+
+# d
 write_report(NULL_CHAR*2+chr(7)+NULL_CHAR*5)
 
-# Press RETURN/ENTER key
-write_report(NULL_CHAR*2+chr(40)+NULL_CHAR*5)
-
-# Press e key
-write_report(NULL_CHAR*2+chr(8)+NULL_CHAR*5)
-# Press f key
-write_report(NULL_CHAR*2+chr(9)+NULL_CHAR*5)
+# ! (press shift and 1)
+write_report(chr(32)+NULL_CHAR+chr(30)+NULL_CHAR*5)
 
 # Release all keys
 write_report(NULL_CHAR*8)
