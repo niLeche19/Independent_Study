4820db35893bd45b865f25bf141117b920b8c80f
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-11-07 18:43:23 -0500

    Update hidtest.py

--- hidtest.py
+++ hidtest.py
@@ -17,7 +17,7 @@ write_report(NULL_CHAR*2+chr(8)+NULL_CHAR*5)
 write_report(NULL_CHAR*2+chr(15)+NULL_CHAR*5)
 write_report(NULL_CHAR*8)
 write_report(NULL_CHAR*2+chr(15)+NULL_CHAR*5)
-write_report(NULL_CHAR*2+chr(37)+NULL_CHAR*5)
+write_report(NULL_CHAR*2+chr(CD)+NULL_CHAR*5)
 # o
 write_report(NULL_CHAR*2+chr(18)+NULL_CHAR*5)
 
