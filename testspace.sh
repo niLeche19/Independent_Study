242c149bb3e4536247b1aa930b8692d1636b6f45
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-11-11 12:19:34 -0500

    Update testspace.sh

--- testspace.sh
+++ testspace.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-"""
-function write_report {
-    echo -ne $1 > /dev/hidg0
-}
-"""
+
+#function write_report {
+#    echo -ne $1 > /dev/hidg0
+#}
+
 pins=(2 3 4 17 27 22 10 9 11 20 5 6 13 19 26 21)
 
 for i in ${pins[@]}; do
@@ -11,9 +11,11 @@ for i in ${pins[@]}; do
 	echo "in" > /sys/class/gpio/gpio$i/direction
 done
 
+printf "all the pins are setup"
+
 while true; do
 	for f in ${pins[@]}; do
-		if [$(</sys/class/gpio/gpio$f/value) == 0]; then
+		if [0 == $(</sys/class/gpio/gpio$f/value)]; then
 			printf "pin $f is down"
 		fi
 	done
