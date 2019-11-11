ce82257d7b0199cb2192b52b8d32d90263db4401
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-11-11 13:09:55 -0500

    Update testspace.sh

--- testspace.sh
+++ testspace.sh
@@ -8,9 +8,9 @@ pins=(2 3 4 17 27 22 10 9 11 20 5 6 13 19 26 21)
 
 printf "all the pins are setup"
 
-while [0 == "$(</sys/class/gpio/gpio13/value)"]; do
+while [1=="$(</sys/class/gpio/gpio13/value)"]; do
 	for f in ${pins[@]}; do
-		if [0 == "$(</sys/class/gpio/gpio"$f"/value)" ]; then
+		if [0=="$(</sys/class/gpio/gpio"$f"/value)" ]; then
 			printf "pin $f is down"
 		fi
 	done
