58c00f67db3cd823f38d37937525210d8b32f320
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-11-12 18:51:37 -0500

    Update testspace.sh

--- testspace.sh
+++ testspace.sh
@@ -11,7 +11,7 @@ printf "all the pins are setup"
 #while $[0==$(</sys/class/gpio/gpio13/value)]; do
 while true; do
 	for f in ${pins[@]}; do
-		if [ 1=="$(</sys/class/gpio/gpio"$f"/value)" ]; then
+		if [ 1 == "$(</sys/class/gpio/gpio"$f"/value)" ]; then
 			printf "pin $f is down\n"
 		else
 			printf "CLOSED\n"
