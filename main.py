4f1f06d2cd1be71db6d2a20fc888229c45e230b5
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-11-17 20:20:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,24 @@ import RPi.GPIO as iop
 
 iop.setmode(iop.BCM)
 pins = (27,2,3,4,17,22,10,9,11,5,6,13,26,21,20)
-tstpins = (2,11,13,27)
+tstpins = (2,27,11,13)
 keysdown = []
 
 for i in pins:
 	iop.setup(i, iop.IN, pull_up_down=iop.PUD_UP)
 print ("Pins are set up, starting now :)")
+
+# define each set of strokes for each key hear:
+def one():
+	print(1)
+def five():
+	print(1)
+def nine():
+	print(9)
+def thirteen():
+	print(13)
+functions = (one, five, nine, thriteen)
+
 def lstscn (l, n):
 	fls = 0
 	
@@ -35,19 +47,23 @@ try:
 	while True:
 		#pressed = 0
 		
-		for i in tstpins:
+		for i in range(len(tstpins)):
+			newi = tstpins[i]
 			fals = 1
-			if iop.input(i) == 0: #checks list for pressed keys
+			if iop.input(newi) == 0: #checks list for pressed keys
 				#pressed = 1
-				if lstscn (keysdown, i) == False:
-					print (i)
-					if i == 2:
+				if lstscn (keysdown, newi) == False:
+					print (newi + "yip dip")
+					functions[i]()
+					"""
+					if newi == 2:
 						system('sudo python hidtest.py')
-					keysdown.append(i)
+					"""
+					keysdown.append(newi)
 					
-			elif iop.input(i)==1: #checks list for released keys
-				if lstscn(keysdown, i) == True:
-					keysdown.remove(i)
+			elif iop.input(newi)==1: #checks list for released keys
+				if lstscn(keysdown, newi) == True:
+					keysdown.remove(newi)
 
 finally:
 	print ("Goodbye!")
