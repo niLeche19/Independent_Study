56c855548a347a9b6435c57127587c46ce99776a
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-11-17 20:47:26 -0500

    Delete givekeys.py

--- givekeys.py
+++
@@ -1,5 +0,0 @@
-"""//\\//\\//\\//\\//\\//\\//\\//\\
-This file will take a string of key inputs and tell you what you will need to input into the next step.
-//\\//\\//\\//\\//\\//\\//\\//\\"""
-import pynput
-#from pynput.keyboard import Key, Controller
