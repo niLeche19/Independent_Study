a4938db41ccb226a0007fece906890cce1871e1c
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-11-22 20:23:15 -0500

    1-13 keys done

--- keys.py
+++ keys.py
@@ -1,10 +1,41 @@
 def one():
 	sendit(0, 30)
 	sendit(32, 30)
-
+	
+def two():
+	sendit(32, 9)
+	
+def three():
+	sendit(32, 9)
+	
+def four():
+	sendit(32, 9)
+	
 def five():
 	sendit(16, 23)
+	
+def six():
+	sendit(32, 9)
+	
+def seven():
+	sendit(32, 9)
+	
+def eight():
+	sendit(32, 9)
+	
 def nine():
 	sendit(16, 26)
+	
+def ten():
+	sendit(32, 9)
+	
+def eleven():
+	sendit(32, 9)
+	
+def twelve():
+	sendit(32, 9)
+	
 def thirteen():
 	sendit(0, 232)
+	
+functions = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thriteen]
