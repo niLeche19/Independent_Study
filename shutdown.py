02dcf5c90a089f4aa151dc90de77537586fffbe9
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-11-27 17:43:49 -0500

    setup win/OSX switch

--- shutdown.py
+++ shutdown.py
@@ -4,6 +4,8 @@ from os import system
 
 iop.setmode(iop.BCM)
 iop.setup(16, iop.IN, pull_up_down=iop.PUD_UP)
+iop.setup(12, iop.IN, pull_up_down=iop.PUD_UP)
+
 while True:
 	if iop.input(16) == 0:
 		sleep(0.3)
