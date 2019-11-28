a6ee13ef203bd9e97ca5b749c70e11f4db6c842c
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-11-28 13:28:35 -0500

    OS pin is now 18

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ keysdown = []
 for i in pins:
 	iop.setup(i, iop.IN, pull_up_down=iop.PUD_UP)
 
-iop.setup(15, iop.IN, pull_up_down=iop.PUD_UP) # 12 is setup seperatly for OS toggle switch
+iop.setup(18, iop.IN, pull_up_down=iop.PUD_UP) # 12 is setup seperatly for OS toggle switch
 
 print ("Pins are set up, starting now :)")
 
@@ -54,10 +54,10 @@ def lstscn (l, n):
 try:
 	while True:
 		sleep(0.05)
-		print(iop.input(15), keys.OS)
-		if iop.input(15) == 0: 
+		print(iop.input(18), keys.OS)
+		if iop.input(18) == 0: 
 			keys.OS = 0
-		elif iop.input(15) == 1:
+		elif iop.input(18) == 1:
 			keys.OS = 1
 		
 		for i in range(len(pins)):
