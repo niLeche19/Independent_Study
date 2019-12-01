ffe7d104e2f61fbbdde3679275cac5794b9d5c41
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-12-01 17:53:49 -0500

    relapse

--- pinconfig.py
+++ pinconfig.py
@@ -4,22 +4,25 @@ import RPi.GPIO as iop
 screen = 1
 
 def initiate():
-	forwardd = input(" Welcome to the key configurator. \n At any time you can press 'h' for help or 'b' to go back.")
+	forwardd = input(" Welcome to the key configurator. \n At any time you can press 'h' for help or 'b' to go back.\n")
 	screen = 1
 	screenone()
 	
 def screenone():
 	direc = input("")
 	if direc == 'h':
-		print("h   : Help. \nq   : Quit.\ne   : Edit a key.")
+		print(" h   : Help. \n q   : Quit.\n e   : Edit a key.")
 		screenone()
-	if direc == 'q':
+	elif direc == 'q':
 		print("Seeya later.")
-	if direc == 'e':
+	elif direc == 'e':
 		whichh = input("What key would you like to edit?")
 		if whichh > 16 or whichh < 1:
 			print("Please enter a number (1-16)")
 		else: screentwo(whichh)
+	else:
+		print("Press 'h' for help.")
+		screenone()
 	
 def screentwo(key):
 	return('')
