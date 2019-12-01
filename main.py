1ffaa46173b9dcd52c48b794daa5b6f8effe576f
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-12-01 18:03:28 -0500

    added cansend var

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Project: Macro keyboard							#
 from time import sleep
 from os import system
 import RPi.GPIO as iop
-import keys
+import keys, pinconfig
 
 iop.setmode(iop.BCM)
 NULL_CHAR = chr(0)
@@ -18,12 +18,13 @@ pins = (2,3,4,17,27,22,10,9,11,20,5,6,13,19,26,21)
 tstpins = (2,27,11,13)
 
 keysdown = []
+cansend = 1
 
 for i in pins:
 	iop.setup(i, iop.IN, pull_up_down=iop.PUD_UP)
 
-iop.setup(18, iop.IN, pull_up_down=iop.PUD_UP) # 12 is setup seperatly for OS toggle switch
-
+iop.setup(18, iop.IN, pull_up_down=iop.PUD_UP) # 18 is setup seperatly for OS toggle switch
+#iop.setup(PIN, iop.IN, pull_up_down=iop.PUD_UP) # this is setup seperatly for pin config enter
 print ("Pins are set up, starting now :)")
 
 """
@@ -55,6 +56,10 @@ try:
 	while True:
 		sleep(0.09)
 		#print(iop.input(18), keys.OS)
+		if iop.input(PIN) == 0:
+			while iop.input(PIN) == 0:
+				time.sleep(0.05)
+			
 		if iop.input(18) == 0: 
 			keys.OS = 1
 		elif iop.input(18) == 1:
@@ -66,7 +71,8 @@ try:
 			# operating system selector
 			if iop.input(newi) == 0: #checks list for pressed keys
 				if lstscn (keysdown, newi) == False:
-					keys.functions[i]()
+					if cansend == 1:
+						keys.functions[i]()
 					keysdown.append(newi)
 					
 			elif iop.input(newi) == 1: #checks list for released keys
