895aac407e7c21818fa4ac5eb2c14f9b7a90efc2
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-12-01 17:59:48 -0500

    added back

--- pinconfig.py
+++ pinconfig.py
@@ -11,16 +11,22 @@ def initiate():
 def screenone():
 	direc = input(" ")
 	if direc == 'h':
-		print(" h   : Help. \n q   : Quit.\n e   : Edit a key.")
+		print(" h   : Help. \n q   : Quit.\n e   : Edit a key.\n b   : Go back to main screen.")
 		screenone()
+	
 	elif direc == 'q':
 		print(" Seeya later.")
+		
+	elif direc == 'b':
+		initiate()
+		
 	elif direc == 'e':
 		whichh = input(" What key would you like to edit?\n ")
 		whichh = int(whichh)
 		if whichh > 16 or whichh < 1:
 			print(" Please enter a number (1-16)\n ")
 		else: screentwo(whichh)
+	
 	else:
 		print(" Press 'h' for help.")
 		screenone()
