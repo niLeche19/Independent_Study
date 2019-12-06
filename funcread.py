39567cc4788d59453612c2105c4237fcee819929
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-12-06 15:29:09 -0500

    reads txt line and returs list var

---
+++ funcread.py
@@ -0,0 +1,24 @@
+
+def readit(line):
+	with open('kc.txt', 'r') as f:
+		lst1 = []
+		lst2 = []
+		rept = f.readlines()
+		f.seek(0)
+		ii = 0
+		#determine how many functions are contained in the line and add them to 
+		for j in rept[line]:
+			if j == '-':
+				lst2.append([])
+		#
+		for h in rept[line]:
+			if h != '-' and h != ',':
+				lst1 += str(h)
+			elif h == ',':
+				lst2[ii].append(int(''.join(lst1)))
+				del lst1[:]
+			elif h == '-':
+				lst2[ii].append(int(''.join(lst1)))
+				del lst1[:]
+				ii += 1
+	return(lst2)
