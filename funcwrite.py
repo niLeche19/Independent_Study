8b5ff6d4bd7ddd849d85b51b45ed2f3789b93b31
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-12-14 13:07:03 -0500

    changes and writes the needed line

--- funcwrite.py
+++ funcwrite.py
@@ -1,3 +1,17 @@
+
+convert = ""
+
+def writeit(line, content):
+	with open('kc.txt', 'r') as rr:
+		lines = rr.readlines()
+
+	for i in content:
+		convert += ("{},{}-\n".format(i[0], i[1]))
 	
-	
-	def writeit(line, www)
+	del lines[line - 1]
+	lines.insert(line - 1, convert)
+
+	with open('kc.txt', 'w') as ww:
+		ww.writelines(lines)
+
+		
