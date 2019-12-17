0eaa5288c4b9fef5a507c6d2d2dbaa5ca2f54959
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-12-16 22:27:16 -0500

    Update pinconfig.py

--- pinconfig.py
+++ pinconfig.py
@@ -63,6 +63,7 @@ def screentwo(key):
 		if input(" Are you sure? (y/n)\n ") == 'y':
 			#tmplst = funcread.readit(key)
 			funcwrite.writeit(key, [[0,0]])
+			print(tmplst)
 			#del lines[key - 1]
 			#lines.insert((key - 1), '0,0-\n')
 		else:
