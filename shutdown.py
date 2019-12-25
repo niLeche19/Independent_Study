8ca10c0a7bde43dd493b2f116bf88a6cc91e4db8
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-12-25 16:05:52 -0500

    config testing

--- shutdown.py
+++ shutdown.py
@@ -1,11 +1,21 @@
 import RPi.GPIO as iop
 from time import sleep
 from os import system
+import pinconfig
 
 iop.setmode(iop.BCM)
 iop.setup(16, iop.IN, pull_up_down=iop.PUD_UP)
+iop.setup(23, iop.IN, pull_up_down=iop.PUD_UP)
+
+cansend = 1
 
 while True:
+	if iop.input(23) == 0 and cansend == 1:
+		while iop.input(23) == 0:
+			time.sleep(0.02)
+			#cansend = 0
+		pinconfig.initiate()
+	
 	if iop.input(16) == 0:
 		sleep(0.3)
 		if iop.input(16) == 0:
