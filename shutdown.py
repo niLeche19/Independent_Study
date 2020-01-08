0ec0d64450b5b8502cde9b6ebfb331ceed26f2a2
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2020-01-07 20:37:18 -0500

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -3,12 +3,12 @@ from time import sleep
 from os import system
 
 iop.setmode(iop.BCM)
-iop.setup(16, iop.IN, pull_up_down=iop.PUD_UP)
+iop.setup(15, iop.IN, pull_up_down=iop.PUD_UP)
 
 while True:
 	sleep(0.01)
-	if iop.input(16) == 0:
+	if iop.input(15) == 0:
 		sleep(0.3)
-		if iop.input(16) == 0:
+		if iop.input(15) == 0:
 			system('sudo shutdown now')
 iop.cleanup
