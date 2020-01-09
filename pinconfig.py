c757ec23502986b51865a90749a5210ceb591d63
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2020-01-08 22:25:16 -0500

    text aditions

--- pinconfig.py
+++ pinconfig.py
@@ -163,7 +163,7 @@ def screentwo(key):
 	elif nextt == 'mc':
 		if len(tmplst) > 1:
 			try:
-				wheree = int(input(" Where would like to add the new key instance? (1-{})\n ".format(len(tmplst) - 1)))
+				wheree = int(input(" Where would like to add the new key instance? (1-{})\n ".format(len(tmplst))))
 			except:
 				print(" Please input a number 1 - {}\n".format(len(tmplst)))
 				screentwo(key)
